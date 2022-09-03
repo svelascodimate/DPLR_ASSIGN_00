e0d483ffcc7f4c07359167481ac8cb79aa23c648
Alejandro Velasco <mordreth@Ishtar.local>
2022-09-03 15:08:58 -0400

    updated rank function

--- main.py
+++ main.py
@@ -2,6 +2,6 @@ import pandas as pd
 
 if __name__ == '__main__':
     people_information = pd.read_csv('./data/data.csv')
-    names_asc = people_information.sort_values(by=['Name'])['Name']
-    print(names_asc)
+    people_information['Rank'] = people_information['Name'].rank(ascending=True)
+    print(people_information)
 
